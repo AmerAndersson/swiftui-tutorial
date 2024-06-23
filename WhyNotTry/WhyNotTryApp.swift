39e417a507577dfe02b7ed43cd261bf0360c87e1
Amer Andersson <anderssonamer@outlook.com>
2024-06-23 14:24:16 +0200

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Amer Andersson on 2024-06-23.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
